0133cff549b19a764ee7fcfa4f3a1d733dfae14b
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2019-10-08 17:28:50 -0700

    Updated myTests to use current code

--- myTests.ml
+++ myTests.ml
@@ -1,37 +1,34 @@
-(*
- *  Fill in `myTestList` with your own tests. There are two ways to add a test:
+open Runner
+open Expr
+open Printf
+open OUnit2
+
+(* Fill in `myTestList` with your own tests. There are two ways to add a test:
  *
- *  (1) By adding the code of the program as a string. In this case add an entry
- *      of this form in `myTestList`:
+ * (1) By adding the code of the program as a string. In this case, add an entry
+ *     of this form in `myTestList`:
  *
- *      (Code (<name>, <program>), <result>)
+ *     t <test_name> <program_code> <result>
  *
- *      For example:
+ * (2) By adding a test inside the 'input/' folder and adding an entry for it
+ *     in `myTestList`. The entry in this case should be:
  *
- *      (Code ("Additions", "5 + (10 + 20)"), "35")
+ *     t_file <test_name> <file_name> <result>
+ *     
+ *     Where name is the name of the file inside 'input/' with the extension
+ *     ".ana". For example:
  *
- *  (2) By adding a test inside the 'input/' folder and adding an entry for it
- *      in `myTestList`. The entry in this case should be:
- *
- *      (File <name>, <result>)
- *
- *      Where name is the name of the file inside 'input/' without the extension
- *      ".ana". For example:
- *
- *      (File "mytest", "6");
- *
- *      Use a semicolon to separate your individual tests.
+ *     t_file "myTest" "mytest.ana" "6";
  *)
 
+let t_i name program expected args = name>::test_run program name expected args
+let t name program expected = name>::test_run program name expected []
+let terr_i name program expected args = name>::test_err program name expected args
+let t_err name program expected = name>::test_err program name expected []
+let t_parse name program expected =
+  name>::(fun _ -> assert_equal expected (Runner.parse_string program));;
+
 let myTestList =
- [ (* Fill in your tests here: *)
-
- ]
-;;
-
-(*
-let testFailList =
-  [
+  [ (* Fill in your tests here: *)
   ]
 ;;
-*)
